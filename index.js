e3f77f3fab0c0e16e2ec49eb4d228164e315a7ed
mgningithub <mobius1.missile.missile@gmail.com>
2020-07-05 15:22:26 +0900

    modify ui, addd clear function

--- index.js
+++ index.js
@@ -7,8 +7,88 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-function onConnection(socket){
+// システム(ダミープレイヤー)
+const system = {
+  name: "SYSTEM",
+  color: "pink"
+}
+
+// プレイヤーリスト
+let players = {};
+
+// プレイヤークラス
+class Player {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+    this.loginTime = Date.now();
+    this.color = getPaleColor();
+  }
+}
+
+/** 淡い色を取得。プレイヤー背景色として使用。 */
+function getPaleColor() {
+  let v1 = 360 * Math.random();
+  let v2 = 25 + 70 * Math.random();
+  let v3 = 80 + 10 * Math.random();
+  return `hsl(${v1}, ${v2}%, ${v3}%)`;
+}
+
+/** クライアントへのレスポンス */
+function onConnection(socket) {
+  // 描画
   socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+  // プレイヤー発言
+  socket.on('player_say', (data) => io.emit('say',
+    {
+      sender: players[socket.id],
+      message: data,
+      ts: Date.now()
+    }
+  ))
+  // キャンバスクリア
+  socket.on('clear', () => {
+    io.emit('clear');
+    // io.emit('say',
+    //   {
+    //     sender: system,
+    //     message: players[socket.id].name + " cleared the canvas.",
+    //     ts: Date.now()
+    //   })
+  })
+  // 接続時
+  socket.on('join', (name) => {
+    // プレイヤーリストに追加
+    if (!name) { name = "Guest" };
+    let player = new Player(socket.id, name);
+    players[socket.id] = player;
+    console.log('join', player);
+    // システムメッセージ発信
+    io.emit('say',
+      {
+        sender: system,
+        message: players[socket.id].name + " has joined ! 👏",
+        ts: Date.now()
+      })
+    // プレイヤーリスト発信
+    io.emit('players', JSON.stringify(players));
+  })
+  // 切断時
+  socket.on('disconnect', () => {
+    console.log('disconnect', players[socket.id]);
+    if (!players[socket.id]) { return; };
+    // システムメッセージ発信
+    io.emit('say',
+      {
+        sender: system,
+        message: players[socket.id].name + " has left ... 👋",
+        ts: Date.now()
+      })
+    // プレイヤーリストから削除
+    delete players[socke.id];
+    // プレイヤーリスト発信
+    io.emit('players', JSON.stringify(players));
+  })
 }
 
 io.on('connection', onConnection);
--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-(function() {
+(function () {
 
   var socket = io();
   var canvas = document.getElementsByClassName('whiteboard')[0];
@@ -16,24 +16,20 @@
   canvas.addEventListener('mouseup', onMouseUp, false);
   canvas.addEventListener('mouseout', onMouseUp, false);
   canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
-  
+
   //Touch support for mobile devices
   canvas.addEventListener('touchstart', onMouseDown, false);
   canvas.addEventListener('touchend', onMouseUp, false);
   canvas.addEventListener('touchcancel', onMouseUp, false);
   canvas.addEventListener('touchmove', throttle(onMouseMove, 10), false);
 
-  for (var i = 0; i < colors.length; i++){
+  for (var i = 0; i < colors.length; i++) {
     colors[i].addEventListener('click', onColorUpdate, false);
   }
 
   socket.on('drawing', onDrawingEvent);
 
-  window.addEventListener('resize', onResize, false);
-  onResize();
-
-
-  function drawLine(x0, y0, x1, y1, color, emit){
+  function drawLine(x0, y0, x1, y1, color, emit) {
     context.beginPath();
     context.moveTo(x0, y0);
     context.lineTo(x1, y1);
@@ -54,34 +50,51 @@
       color: color
     });
   }
-
-  function onMouseDown(e){
-    drawing = true;
-    current.x = e.clientX||e.touches[0].clientX;
-    current.y = e.clientY||e.touches[0].clientY;
+  /**
+   * ポインタのキャンバス基準の座標を取得
+   * @param {event} e - 発生元イベント
+   * @returns {object} - [x,y]
+   */
+  function getPosition(e) {
+    const rect = e.target.getBoundingClientRect();
+    return {
+      x: e.offsetX || (e.touches[0].clientX - window.pageXOffset - rect.left),
+      y: e.offsetY || (e.touches[0].clientY - window.pageYOffset - rect.top)
+    }
   }
 
-  function onMouseUp(e){
+  function onMouseDown(e) {
+    e.preventDefault(); //タッチ操作の既定を無効化
+    drawing = true;
+    let p = getPosition(e);
+    current.x = p.x;
+    current.y = p.y;
+  }
+
+  function onMouseUp(e) {
     if (!drawing) { return; }
     drawing = false;
-    drawLine(current.x, current.y, e.clientX||e.touches[0].clientX, e.clientY||e.touches[0].clientY, current.color, true);
+    if (e.touches) { return false }; // タッチ終了系イベントではオブジェクト取得できない為ここで終了
+    let p = getPosition(e);
+    drawLine(current.x, current.y, p.x, p.y, current.color, true);
   }
 
-  function onMouseMove(e){
+  function onMouseMove(e) {
     if (!drawing) { return; }
-    drawLine(current.x, current.y, e.clientX||e.touches[0].clientX, e.clientY||e.touches[0].clientY, current.color, true);
-    current.x = e.clientX||e.touches[0].clientX;
-    current.y = e.clientY||e.touches[0].clientY;
+    let p = getPosition(e);
+    drawLine(current.x, current.y, p.x, p.y, current.color, true);
+    current.x = p.x;
+    current.y = p.y;
   }
 
-  function onColorUpdate(e){
+  function onColorUpdate(e) {
     current.color = e.target.className.split(' ')[1];
   }
 
   // limit the number of events per second
   function throttle(callback, delay) {
     var previousCall = new Date().getTime();
-    return function() {
+    return function () {
       var time = new Date().getTime();
 
       if ((time - previousCall) >= delay) {
@@ -91,16 +104,15 @@
     };
   }
 
-  function onDrawingEvent(data){
+  function onDrawingEvent(data) {
     var w = canvas.width;
     var h = canvas.height;
     drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
   }
 
-  // make the canvas fill its parent
-  function onResize() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  }
+  // キャンバスクリア
+  let clear = document.getElementById("clear");
+  clear.addEventListener('click', () => { socket.emit('clear') }, false);
+  socket.on('clear', () => { context.clearRect(0, 0, canvas.width, canvas.height) });
 
 })();
