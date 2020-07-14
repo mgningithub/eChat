025c92774a2805b0c27ff1b449eddcf372c7baee
mgn <mobius1.missile.missile@gmail.com>
2020-07-15 00:37:17 +0900

    add changing thickness function

--- public/main.js
+++ public/main.js
@@ -7,7 +7,8 @@
   var context = canvas.getContext('2d');
 
   var current = {
-    color: 'black'
+    color: 'black',
+    thickness: 1
   };
   var drawing = false;
 
@@ -28,12 +29,14 @@
 
   socket.on('drawing', onDrawingEvent);
 
-  function drawLine(x0, y0, x1, y1, color, emit) {
+  function drawLine(x0, y0, x1, y1, color, thickness, emit) {
+    context.lineCap = "round";
+    context.lineJoin = "round";
     context.beginPath();
     context.moveTo(x0, y0);
     context.lineTo(x1, y1);
     context.strokeStyle = color;
-    context.lineWidth = 2;
+    context.lineWidth = thickness * 2;
     context.stroke();
     context.closePath();
 
@@ -46,7 +49,8 @@
       y0: y0 / h,
       x1: x1 / w,
       y1: y1 / h,
-      color: color
+      color: color,
+      thickness: thickness
     });
   }
   /**
@@ -75,20 +79,26 @@
     drawing = false;
     if (e.touches) { return false }; // タッチ終了系イベントではオブジェクト取得できない為ここで終了
     let p = getPosition(e);
-    drawLine(current.x, current.y, p.x, p.y, current.color, true);
+    drawLine(current.x, current.y, p.x, p.y, current.color, current.thickness, true);
   }
 
   function onMouseMove(e) {
     if (!drawing) { return; }
     let p = getPosition(e);
-    drawLine(current.x, current.y, p.x, p.y, current.color, true);
+    drawLine(current.x, current.y, p.x, p.y, current.color, current.thickness, true);
     current.x = p.x;
     current.y = p.y;
   }
 
   function onColorUpdate(e) {
+    for (var i = 0; i < colors.length; i++) {
+      colors[i].style.boxShadow = "none";
+    }
+    e.target.style.boxShadow = "0 0 10px 2px gray";
     current.color = e.target.className.split(' ')[1];
   }
+  // 初期化　黒
+  colors[0].style.boxShadow = "0 0 10px 2px gray";
 
   // limit the number of events per second
   function throttle(callback, delay) {
@@ -106,7 +116,7 @@
   function onDrawingEvent(data) {
     var w = canvas.width;
     var h = canvas.height;
-    drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
+    drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color, data.thickness);
   }
 
   // キャンバスクリア
@@ -119,5 +129,17 @@
   function drawLog(data) {
     data.forEach(log => { onDrawingEvent(log); });
   }
+
+  const thickness_input = document.getElementById('thickness_input');
+  const thickness_output = document.getElementById('thickness_output');
+  thickness_input.addEventListener('input', onInputThickness, false);
+  function onInputThickness(e) {
+    thickness_output.value = thickness_input.value;
+    current.thickness = thickness_input.value;
+  }
+  //初期化
+  thickness_input.value = thickness;
+  thickness_output.value = thickness_input.value;
+  current.thickness = thickness_input.value;
 
 })();
